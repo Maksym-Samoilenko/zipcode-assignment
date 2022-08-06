4fb2e7467ca17ce3df1c93e934d72665a9de9d2f
Maksym-Samoilenko <maksym.samoilenko@gmail.com>
2022-08-06 12:30:11 -0400

    Delete dist directory

--- dist/apollo/data-sources/Zips.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Zips_1 = require("./Zips");
-describe("test", () => {
-    it("should return first object from collection", () => {
-        expect((0, Zips_1.populateZipData)([{ "zip": "20608" }])).toStrictEqual({ zip: { "zip": "20608" } });
-    });
-});
